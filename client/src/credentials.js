4b5d04745456bc625c2a63117ea51d3bc00e72b2
Kimberly Bailey <kimbailey@Kimberlys-iMac.local>
2018-07-17 10:43:08 -0500

    added credentials file, connected to MongoDB using line 24-42 of App.js file, installed Mongoose

---
+++ client/src/credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongo: { 
+        developement: {
+        mongodb:/KimBailey:IKEAForce1@ds137611.mlab.com:37611/ikeafinal,
+    },
+        production: {
+           mongodb:/<KimBailey>:<IKEAForce1>@ds137611.mlab.com:37611/ikeafinal
+        }
+    }
+};
