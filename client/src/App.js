375956545492b1236ed2125161975e4638f4c5cb
Kimberly Bailey <kimbailey@Kimberlys-iMac.local>
2018-08-07 15:19:19 -0500

    removed feed folder and added code to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -40,3 +40,46 @@ switch(app.get('env')){
          throw new Error('Unknown execution environment: ' + 
                           app.get('env'));
 }
+// moved below code from the "Feed" folder that I created last week
+ReactDOM.render (
+  React.createElement('h1',null, 'customerName'),
+  document.getElementById('mongodb://KimBailey:March-319@ds137611.mlab.com:37611/ikeafinal/QuoteFormData')
+);
+
+function previouJobData(customerNames) {
+  return customerName.name.legnth > 0;
+}
+function filterInvalidData(customerNames) {
+  var validData = [];
+  for (var customerName of customerNames) {
+      if (previousJobData (customerName)) {
+          validData.push(customerName);
+      }
+  }
+  return validData;
+}
+
+function customerNameToFormattedString(customerName) {
+  return 'name'; customerName.name;
+}
+
+var customerName = [
+  { 
+      name: 'Isaac Grey',
+      password: 'irockcoding'
+  },
+  {
+      name: 'Kim Bailey',
+      passowrd: 'IKEAForce1'
+  },
+  { 
+      name: 'Anani Vossah',
+      password: 'IKEAForce2'
+  }
+];
+
+var validData = filterInvalidData(customerNames);
+
+for (var i = 0; i < validData.length; i++) {
+  return(cutomerNameToFormattedString(customerNames[i]));
+}
