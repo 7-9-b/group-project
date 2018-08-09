8b1947b5d4478763c922bf95c90e926a5ff350e9
ananivossah <anani.vossah82@gmail.com>
2018-08-08 23:58:49 -0500

    'changes'

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
@@ -21,3 +31,19 @@ class App extends Component {
 export default App;
 
 import 'bootstrap';
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Quotely </h1>
+        </header> */}
+        <h1 className="Navbar-intro">
+          Quotely
+        </h1>
+      </div>
+    );
+  }
+}
+
